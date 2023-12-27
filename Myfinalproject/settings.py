0f732923cf43956d0230c060f66ef53dd32d3ccb
bhagyashriajmire123 <bhagyashriajmire66@gmail.com>
2023-12-27 19:38:08 +0530

    commit all changes

--- Myfinalproject/settings.py
+++ Myfinalproject/settings.py
@@ -118,4 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+DEFAULT_AUTO_FIELD ='django.db.models.BigAutoField'
+
 STATIC_URL = '/static/'
+
